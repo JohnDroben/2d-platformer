93b09f25e72698118ff4aa000b92eb6dbb5e79e5
logg1n <loggiin13@gmail.com>
2025-03-30 14:20:24 +0300

    Класс создания персонажа

--- Characters/Hero/hero.py
+++ Characters/Hero/hero.py
@@ -1,19 +1,10 @@
 from Characters.character import Character
 
 class Hero(Character):
-   def __init__(self, name, speed: float, strength: float):
-      super().__init__(name, speed, strength)
+   def __init__(self, x, y, width, height, speed, jump_force, gravity, ground_level):
+      super().__init__(x, y, width, height, speed, jump_force, gravity, ground_level)
+      self.score = 0
+      self.health = 3
+      self.artifact = 0
 
-   def jump(self):
-      """Логика прыжка и двойного прыжка"""
-      if self.on_ground:
-         # Обычный прыжок с земли
-         self.velocity_y = -self.jump_force
-         self.on_ground = False
-         self.current_action = 'jump'
-         self.can_double_jump = True  # Разрешаем двойной прыжок
-      elif self.can_double_jump:
-         # Двойной прыжок в воздухе
-         self.velocity_y = -self.jump_force
-         self.can_double_jump = False
-         self.current_action = 'jump'
+
